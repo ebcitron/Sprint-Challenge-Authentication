23283e52b71ae06944e8422b6c1913c112490028
Eli Citron <ebcitron@gmail.com>
2019-05-24 11:15:49 -0500

    Getting things goin

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const Users = require('./model');
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -10,10 +12,41 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 5);
+  user.password = hash;
+  
+  Users.add(user)
+  .then(saved => {
+    res.status(201).json(saved);
+  })
+  .catch(error => {
+    res.status(500).json(error);
+  });
 }
 
 function login(req, res) {
+let {username, password } = req.body;
+
+Users.findBy({username})
+.first()
+.then(user => {
+  if (user && bcrypt.compareSync(password, user.password)) {
+    const token = tokenService.generateToken(user);
+    res.status(200).json({
+      message: `Welcome ${user.username}!`,
+      token,
+    });
+  } else {
+    res.status(401).json({ message: "Invalid Credentials"});
+  }
+})
+.catch(error => {
+  console.log('error', error);
+  res.status(500).json(error);
+});
   // implement user login
+
 }
 
 function getJokes(req, res) {
