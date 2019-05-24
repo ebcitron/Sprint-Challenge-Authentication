4c775e98593c2e614567eed9583c86554e77facf
Eli Citron <ebcitron@gmail.com>
2019-05-24 11:31:38 -0500

    Login/Auth/Jokes All Functional

--- config/routes.js
+++ config/routes.js
@@ -1,13 +1,14 @@
-const axios = require('axios');
-const bcrypt = require('bcryptjs');
-const Users = require('./model');
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
+const Users = require("./model");
+const tokenService = require("../auth/tokenService");
 
-const { authenticate } = require('../auth/authenticate');
+const { authenticate } = require("../auth/authenticate");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
 function register(req, res) {
@@ -15,51 +16,50 @@ function register(req, res) {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 5);
   user.password = hash;
-  
+
   Users.add(user)
-  .then(saved => {
-    res.status(201).json(saved);
-  })
-  .catch(error => {
-    res.status(500).json(error);
-  });
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function login(req, res) {
-let {username, password } = req.body;
+  let { username, password } = req.body;
 
-Users.findBy({username})
-.first()
-.then(user => {
-  if (user && bcrypt.compareSync(password, user.password)) {
-    const token = tokenService.generateToken(user);
-    res.status(200).json({
-      message: `Welcome ${user.username}!`,
-      token,
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = tokenService.generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      console.log("error", error);
+      res.status(500).json(error);
     });
-  } else {
-    res.status(401).json({ message: "Invalid Credentials"});
-  }
-})
-.catch(error => {
-  console.log('error', error);
-  res.status(500).json(error);
-});
   // implement user login
-
 }
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" }
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
